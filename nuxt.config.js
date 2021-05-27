e1daa2ab1f62a988d3edbca506d163ec0f541e4e
Nico <nicosiput@gmail.com>
2021-05-27 13:43:05 +0700

    add config server port and ip

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,4 +87,9 @@ export default {
   generate: {
     fallback: true,
   },
+  server: {
+    port: 3000, // default: 3000
+    host: '172.31.28.27', // default: localhost,
+    timing: false,
+  },
 }
